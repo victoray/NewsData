366e903094610af6e6203ff657831b5c58d6d573
Victor A <viktoray007@gmail.com>
2019-06-03 18:08:05 +0300

    add all queries

---
+++ newsdb.py
@@ -0,0 +1,63 @@
+import psycopg2
+import time
+import datetime
+
+dbname = "news"
+
+db = psycopg2.connect(database=dbname)
+
+c = db.cursor()
+
+print("What are the most popular three articles of all time?\n")
+c.execute(
+    "SELECT a.title, COUNT(*) FROM authors JOIN articles a ON authors.id = a.author JOIN log ON log.path = ('/article/' || a.slug) GROUP BY 1 ORDER BY 2 DESC LIMIT 3")
+
+result = [list(item) for item in c.fetchall()]
+for item in result:
+    print("{} - {} views".format(item[0], item[1]))
+
+print("-" * 60)
+
+c.execute(
+    "SELECT authors.name, COUNT(*) FROM authors JOIN articles a ON authors.id = a.author JOIN log ON log.path = ('/article/' || a.slug) GROUP BY 1 ORDER BY 2 DESC")
+
+
+print("\nWho are the most popular article authors of all time?\n")
+result = [list(item) for item in c.fetchall()]
+for item in result:
+    print("{} - {} views".format(item[0], item[1]))
+
+print("-" * 60)
+
+print('\nOn which days did more than 1% of requests lead to errors?\n')
+
+query = '''
+WITH tb1 as (
+    SELECT DATE_TRUNC('day', time) as day, COUNT(*)
+    FROM log
+    WHERE path != '/'
+    GROUP BY 1
+     ),
+
+     tb2 as (
+         SELECT DATE_TRUNC('day', time) as day, COUNT(*)
+         FROM log
+         WHERE status != '200 OK'
+         GROUP BY 1
+     )
+SELECT tb1.day, tb2.count, tb1.count, ((tb2.count * 1./ tb1.count) * 100) as percentage
+FROM tb1,tb2
+WHERE tb1.day = tb2.day
+GROUP BY 1, 2, 3
+HAVING ((tb2.count * 1./ tb1.count) * 100) > 1
+ORDER BY 2 DESC
+'''
+
+c.execute(query)
+result = [list(item) for item in c.fetchall()]
+
+for item in result:
+    print("{} - {:.2f}% errors".format(item[0].strftime('%B %d, %Y'), float(item[3])))
+
+
+db.close()
